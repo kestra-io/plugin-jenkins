775a723fe52f712930dfda97dcc11008396d3965
ben8t <pimpaudben@gmail.com>
2025-12-30 15:38:42 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/jenkins/package-info.java
+++ src/main/java/io/kestra/plugin/jenkins/package-info.java
@@ -1,7 +1,10 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for interacting with Jenkins from Kestra.\n" +
         "Jenkins is an open-source automation server used to build, test, and deploy software projects.",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    categories = PluginSubGroup.PluginCategory.TOOL,
+    categories = {
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.jenkins;
 
