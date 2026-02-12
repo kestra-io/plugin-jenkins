647467f0012b72858e9a17d6891af1fd2af47855
François Delbrayelle <fdelbrayelle@kestra.io>
2026-02-12 11:31:04 +0100

    chore: merge package categories attribute

--- src/main/java/io/kestra/plugin/jenkins/package-info.java
+++ src/main/java/io/kestra/plugin/jenkins/package-info.java
@@ -1,8 +1,6 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for interacting with Jenkins from Kestra.\n" +
-        "Jenkins is an open-source automation server used to build, test, and deploy software projects.",
-    categories = PluginSubGroup.PluginCategory.TOOL,
-    categories = {
+        "Jenkins is an open-source automation server used to build, test, and deploy software projects.",categories = {
         PluginSubGroup.PluginCategory.INFRASTRUCTURE
     }
 )
